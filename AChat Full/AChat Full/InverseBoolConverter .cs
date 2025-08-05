3aca00224b4796326b252e8d2c467409c61c33b8
Nikita Lukanin <breaker1994@mail.ru>
2025-08-05 12:39:55 +0300

    Fix chat page

---
+++ AChat Full/AChat Full/InverseBoolConverter .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AChatFull
+{
+    public class InverseBoolConverter : IValueConverter
+    {
+        // value — входящее значение (object), parameter и culture обычно не используются
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return !b;
+            return true; // или false, в зависимости от логики
+        }
+
+        // Обратное преобразование, если нужно (обычно не применяется)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return !b;
+            return true;
+        }
+    }
+}
