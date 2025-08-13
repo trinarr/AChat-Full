b7004d85290432a2a16912776e296c5c3577d7f4
Nikita Lukanin <breaker1994@mail.ru>
2025-08-13 14:34:31 +0300

    Change name

--- AChat Full/AChat Full.Android/MainActivity.cs
+++ AChat Full/AChat Full.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Plugin.Fingerprint;
 
 namespace AChatFull.Droid
 {
-    [Activity(Label = "AChat_Full", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ICQ2", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
