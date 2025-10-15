7bc6a7bd6eeba04247dde6b303d0298b2f6c57c3
Nikita Lukanin <breaker1994@mail.ru>
2025-10-15 03:43:33 +0300

    Add contact list settings

--- AChat Full/AChat Full/Views/ContactListSettingsPage.xaml.cs
+++ AChat Full/AChat Full/Views/ContactListSettingsPage.xaml.cs
@@ -6,18 +6,30 @@ namespace AChatFull.Views
     public partial class ContactListSettingsPage : ContentPage
     {
         const string ColumnsKey = "contacts.columns";     // int: 1 или 2
-        const string ShowGroupsKey = "contacts.showgroups"; // bool
+        const string ShowGroupsKey = "contacts.showgroups";  // bool
+
+        int _selectedColumns;
+        public int SelectedColumns
+        {
+            get => _selectedColumns;
+            set
+            {
+                if (_selectedColumns == value) return;
+                _selectedColumns = value;
+                OnPropertyChanged(nameof(SelectedColumns));   // обновляет радио-индикаторы
+                Preferences.Set(ColumnsKey, _selectedColumns);
+                // при желании — уведомить ContactsPage:
+                // MessagingCenter.Send(this, "Contacts.ColumnsChanged", _selectedColumns);
+            }
+        }
 
         public ContactListSettingsPage()
         {
             InitializeComponent();
 
-            // загрузим сохранённые настройки (дефолты: 1 колонка, группы видны)
-            var cols = Preferences.Get(ColumnsKey, 1);
-            ColumnsValueLabel.Text = cols.ToString();
-
-            var showGroups = Preferences.Get(ShowGroupsKey, true);
-            ShowGroupsSwitch.IsToggled = showGroups;
+            // init
+            SelectedColumns = Preferences.Get(ColumnsKey, 1);
+            ShowGroupsSwitch.IsToggled = Preferences.Get(ShowGroupsKey, true);
         }
 
         async void OnBackClicked(object sender, System.EventArgs e)
@@ -28,23 +40,17 @@ namespace AChatFull.Views
                 await Navigation.PopAsync();
         }
 
-        async void OnColumnsTapped(object sender, System.EventArgs e)
-        {
-            var choice = await DisplayActionSheet("Number of columns", "Cancel", null, "1", "2");
-            if (choice == "1" || choice == "2")
-            {
-                Preferences.Set(ColumnsKey, int.Parse(choice));
-                ColumnsValueLabel.Text = choice;
+        // Тапы по строкам с радиокнопками
+        void OnOneColumnTapped(object sender, System.EventArgs e) => SelectedColumns = 1;
+        void OnTwoColumnsTapped(object sender, System.EventArgs e) => SelectedColumns = 2;
 
-                // TODO: при желании — уведомить ContactsPage через MessagingCenter
-                // MessagingCenter.Send(this, "Contacts.ColumnsChanged", int.Parse(choice));
-            }
-        }
+        // Show groups
+        void OnShowGroupsTapped(object sender, System.EventArgs e)
+            => ShowGroupsSwitch.IsToggled = !ShowGroupsSwitch.IsToggled;
 
         void OnShowGroupsToggled(object sender, ToggledEventArgs e)
         {
             Preferences.Set(ShowGroupsKey, e.Value);
-            // TODO: при желании — уведомить ContactsPage через MessagingCenter
             // MessagingCenter.Send(this, "Contacts.ShowGroupsChanged", e.Value);
         }
     }
